3bcfbf9d60f9c2f8e671017d285c3442061b664d
dryxoxo <ibrohimhuzaimi@gmail.com>
2023-03-28 15:08:39 +0700

    create landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+import Nav from "../components/Nav";
+
+const Landing = () => {
+  return (
+    <>
+      <Layout>
+        <Nav />
+      </Layout>
+    </>
+  );
+};
+
+export default Landing;
